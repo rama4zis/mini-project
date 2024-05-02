ca959fdef6f3980825c0d62cc5256db596ad61ca
rama <rama.4ditya3@gmail.com>
2024-05-02 13:52:11 +0700

    Add cn function

--- weather-app/src/utils/cn.ts
+++ weather-app/src/utils/cn.ts
@@ -1,3 +1,6 @@
-export function cn(){
-    return 
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]){
+    return twMerge(clsx(...inputs))
 }
